7b0c783017c54fd7b5848b5660ca6bd0325352d8
OPP-studio-macchino <opp.studio67@gmail.com>
2022-12-25 17:53:35 +0900

    1st commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import {
+	GoogleOAuthProvider,
+	GoogleLogin,
+	googleLogout,
+} from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+
+const Login = () => {
+
+    const navigate = useNavigate();
+
+    const getUser = async(response) => {
+        const decoded = jwt_decode(response.credencial);
+        localStorage.setItem('user', JSON.stringify(decoded));
+        console.log(decoded);
+        // const { name, picture, sub, email } = decoded;
+
+        navigate("/", { replace: true });
+    }
+
+    return (
+        <GoogleOAuthProvider clientId={process.env.GOOGLE_API_TOKEN}>
+            <GoogleLogin
+                onSuccess={(response) => getUser(response)}
+                onFailure={() => console.log('Error')}
+                cookiePolicy='single_host_origin'
+                ux_mode='popup'
+            />
+            <div>Login</div>
+        </GoogleOAuthProvider>
+    );
+}
+
+export default Login
---
+++ src/components/integrate.js
@@ -0,0 +1,2 @@
+export { default as Main } from "../components/Main";
+export { default as Login } from "../components/Login";
